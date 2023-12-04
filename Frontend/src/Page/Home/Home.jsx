b8b5f7bd9884e99ab330e46ec44d2719a27007e6
rachit <curiousrachit.26@gmail.com>
2023-12-04 21:17:55 +0530

    Created Routes Pages

---
+++ Frontend/src/Page/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../../Components/Side Bar/SideBar'
+
+function Home() {
+  return (
+    <div>Home
+
+
+        <Sidebar/>
+    </div>
+  )
+}
+
+export default Home
