c8e9480ba485e40844dbd248c3360f295e9d5df0
rachit <curiousrachit.26@gmail.com>
2023-12-05 12:12:24 +0530

    created lay out

---
+++ Frontend/src/Components/LayOut/LayOut.jsx
@@ -0,0 +1,32 @@
+import TopNavbar from '../TopNavBar/TopNav'
+import SideBar from '../../Components/Side Bar//SideBar'
+
+function Layout({ children }) {
+    return (
+        <div>
+
+            {/* Mobile Navbar hidden on desktop  */}
+            <div className=" md:hidden">
+                <TopNavbar />
+            </div>
+            
+            <div class="max-h-screen flex ">
+
+                {/* Sidebar hidden on mobile */}
+    <nav className="w-72 flex-none hidden md:block lg:hidden xl:block">
+      <SideBar />
+    </nav>
+
+                {/* main content  */}
+                <main class="flex-1 min-w-0 overflow-auto ...">
+                    <div className=" flex  justify-center">
+                        {children}
+                    </div>
+                </main>
+
+            </div>
+        </div>
+    )
+}
+
+export default Layout
