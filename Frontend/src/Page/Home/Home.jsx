508bfe541df9d4ee0affe7cd0c170a2a29c325c2
rachit <curiousrachit.26@gmail.com>
2023-12-16 23:01:29 +0530

    used a notes componet

--- Frontend/src/Page/Home/Home.jsx
+++ Frontend/src/Page/Home/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 // import Sidebar from '../../Components/Side Bar/SideBar'
 import Layout from '../../Components/LayOut/LayOut'
+import Notes from '../../Components/Notes/Notes'
 // import Sidebar from '../../Components/Side Bar/SideBar'
 
 function Home() {
   return (
    <Layout>
+<div className='flex  flex-col' >
 
+<Notes/>
+<Notes/>
+<Notes/>
+<Notes/>
+<Notes/>
+</div>
 
    </Layout>
   )
