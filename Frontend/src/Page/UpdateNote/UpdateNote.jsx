2931bdd9313dca7499245ffbebfa8b685751f099
rachit <curiousrachit.26@gmail.com>
2023-12-27 22:32:06 +0530

    changed

--- Frontend/src/Page/UpdateNote/UpdateNote.jsx
+++ Frontend/src/Page/UpdateNote/UpdateNote.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function UpdateNote() {
+
+const [title,setTitle]=useState('');
+const [tag,setTag]=useState('');
+const [description,setDescription]=useState('');
+
+
+
+    //* Get Note By Id
+    const getNotesById = async () => {
+      const res = await fetch(`http://localhost:4000/api/notes/notes/${id}`, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'auth-token': localStorage.getItem('token')
+          }
+      });
+      const data = await res.json();
+
+      //* Set data
+      setTitle(data?.title)
+      setTag(data?.tag)
+      setDescription(data?.description)
+  }
+
+
   return (
     <div>UpdateNote</div>
   )
